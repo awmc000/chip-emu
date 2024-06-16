82736a1ed106d271542632c628c2252694930930
Alex McColm <mccolmalex@gmail.com>
2024-06-16 16:37:31 -0700

    Take filename as argument, extracted frontend functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <fstream>
-#include <ctime>
+#include <chrono>
 #include <string>
 
 #define SCREEN_WIDTH 640
@@ -22,29 +22,56 @@ byte * loadFileBuf(const std::string &filename) {
 	return fileBuf;
 }
 
-void emulate(SDL_Window * window, SDL_Surface * surface) {
+
+void configurePixels(SDL_Rect pixels[32][64], int pixLength)
+{
+    for (int y = 0; y < CHIP8_SCREEN_HEIGHT; y++) {
+        for (int x = 0; x < CHIP8_SCREEN_WIDTH; x++) {
+            pixels[y][x] = {x * pixLength, y * pixLength, pixLength, pixLength};
+        }
+    }
+}
+
+void drawFromChip(Chip8 *sys, SDL_Surface *surface, SDL_Rect pixels[32][64])
+{
+    sys->draw = false;
+
+    for (int y = 0; y < CHIP8_SCREEN_HEIGHT; y++) {
+        for (int x = 0; x < CHIP8_SCREEN_WIDTH; x++) {
+            byte draw = sys->displayBuffer[y][x];
+
+            if (draw == 0) {
+                SDL_FillRect(surface,
+                    &pixels[y][x],
+                    SDL_MapRGB(surface->format, 0x00, 0x00, 0x00));
+            } else {
+                SDL_FillRect(surface,
+                    &pixels[y][x],
+                    SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+            }
+		}
+    }
+}
+
+void emulate(SDL_Window * window, SDL_Surface * surface, const char * filename) {
 	SDL_UpdateWindowSurface(window);
 
 	SDL_Event e;
 	bool running = true;
-	clock_t last = clock();
-	clock_t now = 0;
+	auto last = std::chrono::high_resolution_clock::now();
+	auto now = last;
 
 	Chip8 * sys = new Chip8();
 	SDL_Rect pixels[CHIP8_SCREEN_HEIGHT][CHIP8_SCREEN_WIDTH];
 
-	sys->load(loadFileBuf("chip8logo.ch8"));
+	sys->load(loadFileBuf(filename));
 
 	int pixLength = 10;
 	
 	// Set up pixel rectangles: 64x32 10x10px rectangles.
-	for (int y = 0; y < CHIP8_SCREEN_HEIGHT; y++) {
-		for (int x = 0; x < CHIP8_SCREEN_WIDTH; x++) {
-			pixels[y][x] = {x*pixLength, y*pixLength, pixLength, pixLength};
-		}
-	}
+    configurePixels(pixels, pixLength);
 
-	while (running) {
+    while (running) {
 		while (SDL_PollEvent(&e)) {
 			// Check for quit event
 			if (e.type == SDL_QUIT) {
@@ -52,11 +79,12 @@ void emulate(SDL_Window * window, SDL_Surface * surface) {
 			}
 
 			// Check time
-			now = clock();
-
-			// TODO: If a 700th of a second has passed, run next cycle on Chip8
-			if (1) {
-				last = now;
+			auto elapsed = std::chrono::high_resolution_clock::now() - last;
+			auto elapsedMicroseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+			last = std::chrono::high_resolution_clock::now();
+			
+			// If a 700th of a second has passed, run next cycle on Chip8
+			if (elapsedMicroseconds > 1429LL) {
 
 				sys->cycle();
 
@@ -67,29 +95,10 @@ void emulate(SDL_Window * window, SDL_Surface * surface) {
 				}
 				
 				// If the draw flag is set, draw, then unset it
-				if (sys->draw) {
-					sys->draw = false;
-
-					for (int y = 0; y < CHIP8_SCREEN_HEIGHT; y++) {
-						for (int x = 0; x < CHIP8_SCREEN_WIDTH; x++) {
-							byte draw = sys->displayBuffer[y][x];
-
-							if (draw == 0) {
-								SDL_FillRect(surface,
-									&pixels[y][x],
-									SDL_MapRGB(surface->format, 0x00, 0x00, 0x00)
-								);
-							} else {
-								SDL_FillRect(surface,
-									&pixels[y][x],
-									SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF)
-									);
-							}
-						}
-					}
+                if (sys->draw) {
+					drawFromChip(sys, surface, pixels);
+	                SDL_UpdateWindowSurface(window);
 				}
-
-				SDL_UpdateWindowSurface(window);
 			}
 		}
 	}
@@ -97,9 +106,14 @@ void emulate(SDL_Window * window, SDL_Surface * surface) {
 	delete sys;
 }
 
-int main(void) {
-	
-	SDL_Window * window = nullptr;
+int main(int argc, char ** argv)
+{
+	if (argc < 2) {
+		std::cout << "Include the filename of a Chip8 ROM." << std::endl;
+		exit(0);
+	}
+
+    SDL_Window * window = nullptr;
 	SDL_Surface * surface = nullptr;
 	
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -121,7 +135,8 @@ int main(void) {
 				SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF)
 			);
 	
-			emulate(window, surface);
+			std::string fn = argv[1];
+			emulate(window, surface, fn.c_str());
 		}
 
 	}
