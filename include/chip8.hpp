0a0af934b450f6ff8ab562072e925f02b1501065
Alex McColm <mccolmalex@gmail.com>
2024-06-22 21:07:41 -0700

    Comments with opcodes for instruction functions

--- include/chip8.hpp
+++ include/chip8.hpp
@@ -19,51 +19,123 @@ word combine(byte leftByte, byte rightByte);
 class Chip8 {
 public:
 //private:
+    
     // Blocks/containers
+    
     byte ram[CHIP8_RAM_BYTES];
     byte variableRegisters[CHIP8_VARIABLE_REGISTERS];
     word stack[CHIP8_STACK_HEIGHT];
+    
     // Words
+    
     byte stackPointer;
     word programCounter;
     word indexRegister;
+    
     // Bytes
+    
     byte delayTimer;
     byte soundTimer;
-    // Private functions for executing instructions
-    void execute(word);
+        
+    void execute(word opcode);
+
+    // 00E0: Clear screen 
     void opClear();
+
+    // 1NNN: Jump
     void opJump(word NNN);
+
+    // 6XNN: Set register VX to value NN
     void opSetRegister(byte X, word NN);
+    
+    // 7XNN: Add
     void opAdd(byte X, word NN);
+
+    // ANNN: Set index
     void opSetIndex(word NNN);
+    
+    // DYXN: Draw
     void opDraw(byte X, byte Y, byte N);
+
+    // 2NNN: Call subroutine
     void opCall(word NNN);
+
+    // 00EE: Return from subroutine
     void opReturn();
+
+    // 3XNN: Skip if VX == NN
     void opSkipByteEqual(byte X, byte NN);
+    
+    // 4XNN: Skip if VX != NN
     void opSkipByteUnequal(byte X, byte NN);
+    
+    // 5XY0: Skip if VX == VY
     void opSkipRegEqual(byte X, byte Y);
+    
+    // 9XY0: Skip if VX != VY
     void opSkipRegUnequal(byte X, byte Y);
+    
+    // 8XY0: Set VX to VY
     void opCopyRegister(byte X, byte Y);
+
+    // 8XY1: Set VX to VX | VY
     void opOr(byte X, byte Y);
+    
+    // 8XY2: Set VX to VX & VY
     void opAnd(byte X, byte Y);
+
+    // 8XY3: Set VX to VX ^ VY
     void opXor(byte X, byte Y);
+
+    // 8XY4: Set VX to VX + VY
     void opAddReg(byte X, byte Y);
+
+    // 8XY5: Set VX to VX - VY
     void opSubLR(byte X, byte Y);
+
+    // 8XY7: Set VX to VY - VX
     void opSubRL(byte X, byte Y);
+
+    // 8XYE: Left shift
     void opLeftShift(byte X, byte Y);
+    
+    // 8XY6: Right shift
     void opRightShift(byte X, byte Y);
+
+    // CXNN: Random number & NN
     void opRandom(byte X, byte NN);
+
+    // EX9E: Skip if key X is pressed
     void opSkipKeyDown(byte X);
+    
+    // EXA1: Skip if key X is not pressed
     void opSkipKeyNotDown(byte X);
+
+    // FX07: Set VX to DT
     void opDelayToReg(byte X);
+
+    // FX15: Set DT to VX
     void opSetDelayTimer(byte X);
+
+    // FX18: Set ST to VX
     void opSetSoundTimer(byte X);
+
+    // FX1E: Add VX to I
     void opAddRegToIndex(byte X);
+
+    // FX0A: Block until a key is pressed
     void opGetKey(byte X);
+
+    // FX29: Set I to the hex char in VX
     void opFontChar(byte X);
+
+    // FX33: Store the decimal digits of VX in I, I+1, and I+2
     void opBinaryCodedDecimal(byte X);
+
+    // FX55: Store registers 0 to X to memory at I.
     void opRegistersToRam(byte X);
+
+    // FX65: Load registers 0 to X from memory at I.
     void opRamToRegisters(byte X);
 //public:
     // Display buffer: displayBuffer[y][x] = T / F
